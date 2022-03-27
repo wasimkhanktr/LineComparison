c59115cdb0a3001f83b74e44ac029e7546050c82
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-27 20:08:16 +0530

    [Wasim] Add: Line Comparison UC-1

---
+++ LineComparison/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2, y1, y2;
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("Length of line is " + length);
+
+        }
+    }
+}
